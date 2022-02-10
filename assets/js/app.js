d9d0f9b04c9907895fbcd4b550450a6fd6305d62
Italo Devoto Ramella <italodr@gmail.com>
2022-02-10 19:09:03 +0100

    Add mouse enter animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-$(document)
+$(document).ready(function () {
+    $(".js-project").on("mouseenter", function () {
+        $(this).siblings().addClass("dimmed");
+    });
+
+    $(".js-project").on("mouseleave", function () {
+        $(".js-project").removeClass("dimmed");
+    });
+});
